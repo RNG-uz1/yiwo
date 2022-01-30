03eeefa30a46bef2fb22fd324dfdc4225834ddfd
why <1577027554@qq.com>
2022-01-30 14:47:05 +0800

    年前上线版

--- 译我code/miniprogram/pages/index/index.js
+++ 译我code/miniprogram/pages/index/index.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    howToUse: 0
   },
 
   //弹窗
@@ -24,91 +24,105 @@ Page({
     })
   },
 
-
-
-  //跳转页面  打卡记录
-  gotuRecording(e) {
-      //页面跳转
-      wx.navigateTo({
-      url: `/pages/recording/index`
-    })  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    var that = this
-    setTimeout(function () {
-      console.log(app.globalData.userInfo)
-      that.setData({
-        user: app.globalData.userInfo
+  //展示使用说明
+  showInfo() {
+    if (this.data.howToUse == 0) {
+      this.setData({
+        howToUse: 1
       })
-    }, 1000)
-
-    //隐藏左上角
-    wx.hideHomeButton({
-      success: function () {
-        console.log("hide home success");
-      },
-      fail: function () {
-        console.log("hide home fail");
-      },
-      complete: function () {
-        console.log("hide home complete");
-      },
-    });
-
-
-
-
-
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  close(){
+    this.setData({
+      howToUse : 0
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
 
-  },
+//跳转页面  打卡记录
+gotuRecording(e) {
+  //页面跳转
+  wx.navigateTo({
+    url: `/pages/recording/index`
+  })
+},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+/**
+ * 生命周期函数--监听页面加载
+ */
+onLoad: function (options) {
 
-  },
+},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+/**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+onReady: function () {},
 
-  },
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow: function () {
+  var that = this
+  setTimeout(function () {
+    console.log(app.globalData.userInfo)
+    that.setData({
+      user: app.globalData.userInfo
+    })
+  }, 1000)
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  //隐藏左上角
+  wx.hideHomeButton({
+    success: function () {
+      console.log("hide home success");
+    },
+    fail: function () {
+      console.log("hide home fail");
+    },
+    complete: function () {
+      console.log("hide home complete");
+    },
+  });
 
-  }
+
+
+
+
+},
+
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide: function () {
+
+},
+
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload: function () {
+
+},
+
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh: function () {
+
+},
+
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom: function () {
+
+},
+
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage: function () {
+
+}
 })
