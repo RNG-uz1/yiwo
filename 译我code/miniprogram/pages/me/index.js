e6490260d387622edc427b62825a1669220f45a3
why <1577027554@qq.com>
2022-02-10 23:51:44 +0800

    3.0

--- 译我code/miniprogram/pages/me/index.js
+++ 译我code/miniprogram/pages/me/index.js
@@ -1,14 +1,59 @@
 const app = getApp()
+const db = wx.cloud.database()  
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    inputValue:"",
+    obtnArry:[]
   },
 
-
+  addinput(e){
+    if (this.data.obtnArry.length==8) {
+      wx.showToast({
+        title: '最多8个标签哦~',
+        icon:'none'
+      })
+    }
+    else{
+      var that=this
+      var inputValue
+      wx.showModal({
+        title: '请输入标签',
+        placeholderText:'不超过4个字',
+        editable:true,
+        success(res){
+          if (res.confirm) {
+              //确定按钮，添加数组达到添加标签的作用
+           inputValue=res.content
+        console.log(inputValue)
+        var obtnArry = that.data.obtnArry;
+        console.log(obtnArry)
+        var newData = { num: obtnArry.length, name: res.content };
+        
+        obtnArry.push(newData);
+        
+        that.setData({
+          obtnArry:obtnArry
+        })
+        console.log(obtnArry)
+      
+        //将输入的标签存入数据库
+        db.collection('label').where({
+          _openid: app.globalData.openid
+        }).update({
+          data:{
+            habit:that.data.obtnArry
+          }
+        })
+      
+          }
+        }
+      })
+    }
+  },
  
   gotoWatch(e) {
     wx.navigateTo({
@@ -67,13 +112,73 @@ Page({
     })
   },
 
+  isDel(e){
+    var that=this
+    wx.showModal({
+      content: '确定要删除此标签吗？',
+      showCancel:true,
+      confirmText :'确定',
+      cancelText:'取消',
+      success(res){
+        if (res.confirm) {
+          var index=e.currentTarget.dataset.index//获取点击的下标
+          var habit=that.data.obtnArry//获得整个数组
+          console.log(habit)
+          var a=habit.splice(index,1)
+          console.log(habit)
+          db.collection('label').where({
+            _openid: app.globalData.openid
+          }).update({
+            data:{
+              habit:habit
+            }
+          })
+          db.collection('label').where({ //查找数据库内openid相同的信息
+            _openid: app.globalData.openid
+          }).get().then(res=>{
+            console.log(habit)
+           that.setData({
+            obtnArry:habit
+           })
+          })
+        }
+      }
+    })
 
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+ 
+    var that=this
     this.setData({
       userInfo : app.globalData.userInfo
+    })
+
+    //检查之前是否有openid
+  db.collection('label').where({ //查找数据库内openid相同的信息
+  _openid: app.globalData.openid
+  }).get({
+  success(res) {
+    if (res.data.length == 0) { //如果在数据库内没有找到相同的openid记录则添加
+      //把用户信息和标签添加到数据库
+      wx.cloud.database().collection('label').add({
+        data: {
+          habit:[]
+         },
+        })
+      }
+    }
+  })
+
+    db.collection('label').where({ //查找数据库内openid相同的信息
+      _openid: app.globalData.openid
+    }).get().then(res=>{
+      console.log(res.data[0].habit)
+     that.setData({
+      obtnArry:res.data[0].habit
+     })
     })
   },
 
